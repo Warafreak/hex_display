3df781f81bb3ef584d19c6878d7bb28a80cecde8
Warafreak <SoerenWeber@gmx.net>
2020-04-20 20:23:36 +0200

    readied effects.py

--- effects.py
+++ effects.py
@@ -1,2 +1,15 @@
 # this file houses the pure animations over the array
+import os
+import time
+import board
+import busio
+import digitalio
 import numpy as np
+import adafruit_tlc5947
+
+#------------------whole array effects------------------#
+
+
+#------------------single panel effects-----------------#
+
+# fade every channel slowly on and then off
